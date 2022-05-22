9f6f93b910c2907688ad3a9b7dca7708cc6acc0b
akhdanzaman1 <akhdanzaman@student.uns.ac.id>
2022-05-23 00:08:13 +0700

    2

--- coba5.py
+++ coba5.py
@@ -1,26 +1,8 @@
-import pandas as pd
-import os
-import datetime
+session = 0
 
-filem = input("pilih film = ")
-pilihantanggal = int(input('pilih tanggal = '))
-pilihanjam = int(input('pilihjam = '))
-datakursi2 = pd.read_csv('datakursi2.csv')
-carkur3 = pd.DataFrame(datakursi2)
-carkur2 = carkur3[carkur3['judul']==filem]
-carkur = carkur2[carkur2['tanggal']==pilihantanggal]
-indekskursi = carkur[carkur['jam']==pilihanjam].index.item()
+def tes():
+    session = 1
+    print(session)
 
-datakursi = pd.read_csv('datakursi2.csv', index_col='kode')
-dfdk = pd.DataFrame(datakursi)
-kursipilihan = input('pilih kursi anda = ')
-dfdk.loc[indekskursi, kursipilihan] = 0
-dfdk.to_csv("datakursi2.csv")
-
-print(indekskursi)
-
-#datakursi = pd.read_csv('datakursi2.csv', index_col='index')
-#dfdk = pd.DataFrame(datakursi)
-#kursipilihan = input('pilih kursi anda = ')
-#dfdk.loc[indekskursi, kursipilihan] = 0
-#dfdk.to_csv("datakursi2.csv")
+tes()
+print (session)
--- cobacekdatacsv.py
+++ cobacekdatacsv.py
@@ -9,6 +9,7 @@ nomor = ""
 waktu = ""
 tanggal = ""
 kodebayar = ""
+session = 0
 
 
 def login():
@@ -23,9 +24,13 @@ def login():
 
   if matching_creds:
     print('success')
+    session = 1
+    return session
   else:
     print('\nYour account is not registered yet!')
     print('please contact admin')
+    session = 0
+    return session
 
 def register():
     userData = pd.read_csv('userdatabase.csv')
@@ -46,146 +51,149 @@ def register():
         print('Username already exist')
         register()
 
+session = int(login())
 
-def inputfilm():
-    filem = input('Film : ')
-    return filem
+while session != 0:
+
+  filem = input('Film : ')
+
+  def movieinfo():
+      daftarfilm = pd.read_csv('film.csv')
+      df = pd.DataFrame(daftarfilm)
+
+      judul= (df['judul'].iloc[pilihfilm()])
+      harga = (df['harga'].iloc[pilihfilm()])
+      durasi = (df['durasi'].iloc[pilihfilm()])
+      
+      fullinfo = '''
+      Judul film \t\t : {0}
+      Harga Tiket \t : {1}
+      Durasi film\t\t : {2}
+      '''
+      print(fullinfo.format(judul,harga,durasi)
+      )
 
 
-def movieinfo():
+  def indeksfilm(x):
     daftarfilm = pd.read_csv('film.csv')
     df = pd.DataFrame(daftarfilm)
+    
+    pilihanfilm = x
+    
+    matching_creds = (len(df[(df.judul == pilihanfilm)]) > 0)
 
+    if matching_creds:
+      indekss = df[df['judul']==pilihanfilm].index.item()
+      return indekss
+    else:
+      print()
 
+  print (indeksfilm(filem))
+  indeksu = (indeksfilm(filem))
 
-    judul= (df['judul'].iloc[pilihfilm()])
-    harga = (df['harga'].iloc[pilihfilm()])
-    durasi = (df['durasi'].iloc[pilihfilm()])
+  def pilihfilm():
+    daftarfilm = pd.read_csv('film.csv')
+    df = pd.DataFrame(daftarfilm)
+    
+    judul= (df['judul'].iloc[indeksu])
+    harga = (df['harga'].iloc[indeksu])
+    durasi = (df['durasi'].iloc[indeksu])
     
     fullinfo = '''
     Judul film \t\t : {0}
-    Harga Tiket \t : {1}
+    Harga Tiket \t\t : {1}
     Durasi film\t\t : {2}
     '''
-    print(fullinfo.format(judul,harga,durasi)
-    )
-
-
-def indeksfilm(x):
-  daftarfilm = pd.read_csv('film.csv')
-  df = pd.DataFrame(daftarfilm)
-  
-  pilihanfilm = x
-  
-  matching_creds = (len(df[(df.judul == pilihanfilm)]) > 0)
-
-  if matching_creds:
-    indekss = df[df['judul']==pilihanfilm].index.item()
-    return indekss
-  else:
-    print()
-
-
-print (indeksfilm(filem))
-indeksu = (indeksfilm(filem))
-
-
-def pilihfilm():
-  daftarfilm = pd.read_csv('film.csv')
-  df = pd.DataFrame(daftarfilm)
-
-
-  
-  judul= (df['judul'].iloc[indeksu])
-  harga = (df['harga'].iloc[indeksu])
-  durasi = (df['durasi'].iloc[indeksu])
-  
-  fullinfo = '''
-  Judul film \t\t : {0}
-  Harga Tiket \t\t : {1}
-  Durasi film\t\t : {2}
-  '''
-  print(fullinfo.format(judul,harga,durasi))
-  
-
-def availableseat():
-    kursifilm = pd.read_csv('datakursi2.csv')
-    dfkf = pd.DataFrame(kursifilm)
-    locfilm = dfkf.loc[indeksu]
-    daftarkursi = locfilm.tolist()
-    print()
-    for a in range (7,len(daftarkursi)):
-        if daftarkursi[a] == 1:
-            print('%d Available'%(a-6))
-        else:
-            print('%d Booked' %(a-6))
-    print()
-
-
-def pickdate():
-  print()
-  time = datetime.datetime.now()
-
-
-def pickseat():
-  pilihantanggal = int(input('pilih tanggal = '))
-  pilihanjam = int(input('pilihjam = '))
-  datakursi2 = pd.read_csv('datakursi2.csv')
-  carkur3 = pd.DataFrame(datakursi2)
-  carkur2 = carkur3[carkur3['judul']==filem]
-  carkur = carkur2[carkur2['tanggal']==pilihantanggal]
-  indekskursi = carkur[carkur['jam']==pilihanjam].index.item()
-
-  datakursi = pd.read_csv('datakursi2.csv', index_col='kode')
-  dfdk = pd.DataFrame(datakursi)
-  kursipilihan = input('pilih kursi anda = ')
-  dfdk.loc[indekskursi, kursipilihan] = 0
-  dfdk.to_csv("datakursi2.csv")
-
-
-
-def konfrimasi():
-  print()
-
-def id_generator(size=15, chars=string.ascii_uppercase + string.digits):
-  return ''.join(random.choice(chars) for _ in range(size))
-
-def pembayaran():
-  datapembelian = pd.read_csv('datapembelian.csv')
-  dfdp = pd.DataFrame(datapembelian)
-  while True:
-    id_generator()
-    if (len(dfdp.loc[dfdp['kodebayar'] == id_generator])) < 1:
-        id = id_generator()
-        break
-        
-  print("Kode bayar")
-  print(id)
-
-def rekapbeli():
-    nopembelian = 0
+    print(fullinfo.format(judul,harga,durasi))
     
-    userData = pd.read_csv('userdatabase.csv')
-    dfud = pd.DataFrame(userData)
-    datafilm = pd.read_csv('film.csv')
-    dfdfi = pd.DataFrame(datafilm)
-    datakursi = pd.read_csv('datakursi2.csv')
+
+  def availableseat():
+      kursifilm = pd.read_csv('datakursi2.csv')
+      dfkf = pd.DataFrame(kursifilm)
+      locfilm = dfkf.loc[indeksu]
+      daftarkursi = locfilm.tolist()
+      print()
+      for a in range (7,len(daftarkursi)):
+          if daftarkursi[a] == 1:
+              print('%d Available'%(a-6))
+          else:
+              print('%d Booked' %(a-6))
+      print()
+
+
+  def pickdate():
+    print()
+    time = datetime.datetime.now()
+
+
+  def pickseat():
+    pilihantanggal = int(input('pilih tanggal = '))
+    pilihanjam = int(input('pilihjam = '))
+    datakursi2 = pd.read_csv('datakursi2.csv')
+    carkur3 = pd.DataFrame(datakursi2)
+    carkur2 = carkur3[carkur3['judul']==filem]
+    carkur = carkur2[carkur2['tanggal']==pilihantanggal]
+    indekskursi = carkur[carkur['jam']==pilihanjam].index.item()
+
+    datakursi = pd.read_csv('datakursi2.csv', index_col='kode')
     dfdk = pd.DataFrame(datakursi)
+    kursipilihan = input('pilih kursi anda = ')
+    dfdk.loc[indekskursi, kursipilihan] = 0
+    dfdk.to_csv("datakursi2.csv")
+
+
+
+  def konfrimasi():
+    print()
+
+  def id_generator(size=15, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+  def pembayaran():
     datapembelian = pd.read_csv('datapembelian.csv')
-    dfdt = pd.DataFrame(datapembelian)
+    dfdp = pd.DataFrame(datapembelian)
+    while True:
+      id_generator()
+      if (len(dfdp.loc[dfdp['kodebayar'] == id_generator])) < 1:
+          id = id_generator()
+          break
+          
+    print("Kode bayar")
+    print(id)
 
-    countrow = dfdt.shape[0]
-    nopembelian = print(f"{(countrow + 1):08d}")
+  def rekapbeli():
+      nopembelian = 0
+      
+      userData = pd.read_csv('userdatabase.csv')
+      dfud = pd.DataFrame(userData)
+      datafilm = pd.read_csv('film.csv')
+      dfdfi = pd.DataFrame(datafilm)
+      datakursi = pd.read_csv('datakursi2.csv')
+      dfdk = pd.DataFrame(datakursi)
+      datapembelian = pd.read_csv('datapembelian.csv')
+      dfdt = pd.DataFrame(datapembelian)
 
-    databelibaru = {'nopembelian' : [nopembelian],
-                'user' : [user],
-                'nomor' : [nomor],
-                'judul' : [pilihanfilm],
-                'waktu' : [waktu],
-                'tanggal' : [tanggal],
-                'kodebayar' : [kodebayar],
-                }
-    inputdatapembelian = pd.DataFrame(databelibaru)
-    inputdatapembelian.to_csv('datapembelian.csv', mode='a', index=False, header=False)
+      countrow = dfdt.shape[0]
+      nopembelian = print(f"{(countrow + 1):08d}")
 
-    print('')
+      databelibaru = {'nopembelian' : [nopembelian],
+                  'user' : [user],
+                  'nomor' : [nomor],
+                  'judul' : [pilihanfilm],
+                  'waktu' : [waktu],
+                  'tanggal' : [tanggal],
+                  'kodebayar' : [kodebayar],
+                  }
+      inputdatapembelian = pd.DataFrame(databelibaru)
+      inputdatapembelian.to_csv('datapembelian.csv', mode='a', index=False, header=False)
+
+      print('')
+
+  pickseat()
+  print()
+  print()
+  print()
+  print("Success")
+  print()
+  print()
+  print()
